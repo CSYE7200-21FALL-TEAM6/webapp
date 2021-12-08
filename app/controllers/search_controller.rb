49cd29d926a06ebeada5cbe9262e34d1668d02ea
ailec0623 <2523017180@qq.com>
2021-12-07 23:04:29 -0500

    7th

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -34,5 +34,15 @@ class SearchController < ApplicationController
     end
 
     def show_result
+        @result_num    =  Array.new(5, 0)
+        @like_count    =  Array.new(5, 0)
+        @forward_count =  Array.new(5, 0)
+        records = Record.where(keyword: params[:query], token: params[:token])
+        records.each { |record|
+            index                  = record.sentiment_res.to_i + 2
+            @result_num[index]     += 1
+            @like_count[index]     += record.like_num
+            @forward_count[index]  += record.retweet_num
+        }
     end
 end
