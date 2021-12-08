67b41fbb3ae4cf965a915b863f6e9fcd5ee7947a
ailec0623 <2523017180@qq.com>
2021-12-07 23:57:26 -0500

    7th

---
+++ app/assets/application.js
@@ -0,0 +1,6 @@
+//= require Chart.bundle
+//= require chartkick
+import "chartkick"
+import "Chart.bundle"
+require("chartkick")
+require("chart.js")
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,8 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "chartkick/chart.js"
-
+require("chartkick")
+require("chart.js")
 //= require highcharts
 //= require chartkick
 Rails.start()
