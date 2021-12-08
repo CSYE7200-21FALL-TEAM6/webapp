2f24f0b8c0bfcfc9f6cb5dfd19961f8a0fbe57f6
ailec0623 <2523017180@qq.com>
2021-12-07 21:54:05 -0500

    7th

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -10,13 +10,11 @@ class SearchController < ApplicationController
         records = Record.where(keyword: params[:query], token: params[:token])
         result_num    =  Array.new(5, "0")
         like_count    =  Array.new(5, "0")
-        reply_count   =  Array.new(5, "0")
         forward_count =  Array.new(5, "0")
         records do |record|
             index = record.result
             result_num[index]     += 1
             like_count[index]     += record.like
-            reply_count[index]    += record.reply
             forward_count[index]  += record.forward
         end
         if params[:type] == "result"
@@ -25,10 +23,6 @@ class SearchController < ApplicationController
         end
         if params[:type] == "like"
             render status: 200, json: like_count
-            return
-        end
-        if params[:type] == "reply"
-            render status: 200, json: reply_count
             return
         end
         if params[:type] == "forward"
