974eee5c7f86843c60d3fcb53ee6902f58a6e502
ailec0623 <2523017180@qq.com>
2021-12-07 23:50:22 -0500

    7th

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,5 @@
 //= require chartkick
 import "chartkick"
 import "Chart.bundle"
+require("chartkick")
+require("chart.js")
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,5 @@
 //= require Chart.bundle
 import "chartkick"
 import "Chart.bundle"
+require("chartkick")
+require("chart.js")
