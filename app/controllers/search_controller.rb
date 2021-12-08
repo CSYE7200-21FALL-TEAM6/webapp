09992b638b3a77f5ea28d82ec7b736237479713f
ailec0623 <2523017180@qq.com>
2021-12-07 22:59:13 -0500

    7th

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,15 +7,16 @@ class SearchController < ApplicationController
     end
 
     def result
-        result_num    =  Array.new(5, "0")
-        like_count    =  Array.new(5, "0")
-        forward_count =  Array.new(5, "0")
-        Record.where(keyword: params[:query], token: params[:token]) do |record|
+        result_num    =  Array.new(5, 0)
+        like_count    =  Array.new(5, 0)
+        forward_count =  Array.new(5, 0)
+        records = Record.where(keyword: params[:query], token: params[:token])
+        records.each { |record|
             index                  = record.sentiment_res.to_i + 2
             result_num[index]     += 1
             like_count[index]     += record.like_num
             forward_count[index]  += record.retweet_num
-        end
+        }
         if params[:type] == "result"
             render status: 200, json: result_num
             return
